26bd748082f03a1fa44417a8e7cdd9e7dee4a0af
Serena Liu <sl362@duke.edu>
2019-01-22 12:28:06 -0500

    changes to FSM for q1

--- FSM_hw1.v
+++ FSM_hw1.v
@@ -4,12 +4,12 @@ module FSM_hw1(w, z, reset, clk);
 	input w, reset,clk;
 	output reg z; 
 
-	localparam A=0, B=1, C=2, D=4, E=5, F=6, G=7;
+	localparam A=0, B=1, C=2, D=3, E=4, F=5, G=6, H=7;
 	reg [2:0] state,next; 
 	
 	always @(posedge clk) begin
 	 if (reset) begin
-	 state <= A; // Initial state
+	 state <= A; 
 	 end else begin
 	 state <= next;
 	 end
@@ -17,32 +17,59 @@ module FSM_hw1(w, z, reset, clk);
 	
 	always @(*) begin
 		next <= state;
-		 z <= 0; 
+		 z <= 0;  
 		 case (state)
-			 A : if (w) next <= C;
-			 else next <= B;
-			 B : if (w) next <= D;
-			 C : if (~w) next <= E;
-			 D : begin
-			 z <= 0;
-			 if (w) next <= C;
-			 else next <= E;
-			 end
-			 E : begin
-			 z <= 1;
-			 if (w) next <= D;
-			 else next <= B;
-			 end
+			 A : 
+				 begin
+				 z<=0;
+				 if (w) next <= B;
+				 else next <= A;
+				 end
+			 
+			 B :
+				 begin
+				 z<=0;
+				 if (w) next <= C;
+				 else next <= D;
+				 end
+				 
+			 C :
+				 begin
+				 z<=0;
+				 if (w) next <= C;
+				 else next <= E;
+				 end
+			
+			 D :
+				 begin
+				 z<=0;
+				 if (w) next <= G;
+				 else next <= A;
+				 end
+				 
+			 E :
+				 begin
+				 z<=0;
+				 if (w) next <= F;
+				 else next <= A;
+				 end
+
 			 F : begin
 				z <= 1;
-				if (w) next <= D;
-				else next <= B;
+				if (w) next <= H;
+				else next <= D;
 			 end
 			 
 			 G : begin
+				 z <= 0;
+				 if (w) next <= H;
+				 else next <= D;
+				end
+			 
+			 H : begin
 				 z <= 1;
-				 if (w) next <= D;
-				 else next <= B;
+				 if (w) next <= C;
+				 else next <= E;
 				end
 			 
 
